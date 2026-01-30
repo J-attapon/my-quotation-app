4324847f94959e6f06719b5d6c0ef7dae0dc8f8c
J_attaphon <52853486+J-attapon@users.noreply.github.com>
2026-01-30 08:29:51 +0000

    scr

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-import viteLogo from '/vite.svg' // หรือ '../../public/vite.svg'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
